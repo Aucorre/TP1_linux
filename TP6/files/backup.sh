f97ad34ead50b0e38c0fbee292cb1a95a96319f7
Aucorre <90307439+Aucorre@users.noreply.github.com>
2021-12-07 21:48:29 +0100

    backup.sh

---
+++ TP6/files/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Archives nextcloud data.
+# audren
+
+
+
+name=("/srv/backup/nexcloud_$(date +"%y%m%d_%H%m%S").tar.gz")
+cd /var/www/
+/usr/bin/tar -czvf "$name" nextcloud/ &> /dev/null
+
+# Success
+echo "Backup "$name" created successfully."
+
+# Write a log
+log_prefix=$(date +"[%y/%m/%d %H:%m:%S]")
+log_line="${log_prefix} Backup "$name" created successfully."
+echo "${log_line}" >> /var/log/backup/backup.log
